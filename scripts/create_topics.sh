6c31cbc240f2020b6a3e3b9bd6bbfe8a45544248
agutlop <gutierrez.lopez.ant@gmail.com>
2016-09-24 20:37:48 +0200

    modified:   scripts/create_topics.sh

--- scripts/create_topics.sh
+++ scripts/create_topics.sh
@@ -1,6 +1,7 @@
 cd $HOME/mesoskafka
 
 ./kafka-mesos.sh broker add 0 --cpus 0.5 --heap 300 --mem 300
+./kafka-mesos.sh broker start 0
 ./kafka-mesos.sh topic add topic-wordapp --broker 0
 ./kafka-mesos.sh topic add topic-wordcountapp --broker 0
 ./kafka-mesos.sh topic add topic-meetuprsvp --broker 0
