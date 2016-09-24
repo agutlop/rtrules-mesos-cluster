56ee8e2f9877de10dc5a7da51cc50a59c692def2
agutlop <gutierrez.lopez.ant@gmail.com>
2016-09-24 22:09:41 +0200

    modified:   scripts/create_topics.sh

--- scripts/create_topics.sh
+++ scripts/create_topics.sh
@@ -1,5 +1,8 @@
 cd $HOME/mesoskafka
 
+#sleep, dejamos a marathon arrancar la app mesos-kafka
+sleep 10
+
 ./kafka-mesos.sh broker add 0 --cpus 0.5 --heap 300 --mem 300
 ./kafka-mesos.sh broker start 0
 ./kafka-mesos.sh topic add topic-wordapp --broker 0
